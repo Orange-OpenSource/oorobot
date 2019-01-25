e30bca3ba56be0d93e8a4d91e00a919224e5a1c5
Matthieu Salvat <matthieu.salvat@orange.com>
2019-01-25 18:22:55 +0100

    fix(ionic): remove unused level mode

--- OoRobotIonicApp/src/pages/blockly/blockly.ts
+++ OoRobotIonicApp/src/pages/blockly/blockly.ts
@@ -33,154 +33,33 @@ export class BlocklyPage {
   lastLevel = '0';
   toolboxes = {
     '0':
-      '<xml id="toolbox" style="display: none">' +
-      '<block type="Start"></block>' +
-      '<block type="UpSimple"></block>' +
-      '<block type="DownSimple"></block>' +
-      '<block type="LeftSimple"></block>' +
-      '<block type="RightSimple"></block>' +
-      '<block type="Loop"></block>' +
-      '<block type="PenUp"></block>' +
-      '<block type="PenDown"></block>' +
-      '<block type="PenColor"></block>' +
-      '<block type="Pause"></block>' +
-      '</xml>',
+    '<xml id="toolbox" style="display: none">' +
+    '<block type="Start"></block>' +
+    '<block type="UpSimple"></block>' +
+    '<block type="DownSimple"></block>' +
+    '<block type="LeftSimple"></block>' +
+    '<block type="RightSimple"></block>' +
+    '<block type="Loop"></block>' +
+    '<block type="PenUp"></block>' +
+    '<block type="PenDown"></block>' +
+    '<block type="PenColor"></block>' +
+    '<block type="Pause"></block>' +
+    '</xml>',
     '1':
-      '<xml id="toolbox" style="display: none">' +
-      '<block type="Start"></block>' +
-      '<block type="Up"></block>' +
-      '<block type="Down"></block>' +
-      '<block type="Left"></block>' +
-      '<block type="Right"></block>' +
-      '<block type="Loop"></block>' +
-      '<block type="PenUp"></block>' +
-      '<block type="PenDown"></block>' +
-      '<block type="PenColor"></block>' +
-      '<block type="CircleRight"></block>' +
-      '<block type="CircleLeft"></block>' +
-      '<block type="Pause"></block>' +
-      '</xml>',
-    '2':
-      '<xml id="toolbox" style="display: none">' +
-      '<block type="Start"></block>' +
-      '<block type="Up"></block>' +
-      '<block type="Down"></block>' +
-      '<block type="Left"></block>' +
-      '<block type="Right"></block>' +
-      '<block type="Loop"></block>' +
-      '<block type="PenUp"></block>' +
-      '<block type="PenDown"></block>' +
-      '<block type="PenColor"></block>' +
-      '<block type="CircleRight"></block>' +
-      '<block type="CircleLeft"></block>' +
-      '<block type="Pause"></block>' +
-      '</xml>',
-    '3':
-      '  <xml id="toolbox" style="display: none">\n' +
-      '    <category name="Logic">\n' +
-      '      <category name="If">\n' +
-      '        <block type="controls_if"></block>\n' +
-      '        <block type="controls_if">\n' +
-      '          <mutation else="1"></mutation>\n' +
-      '        </block>\n' +
-      '        <block type="controls_if">\n' +
-      '          <mutation elseif="1" else="1"></mutation>\n' +
-      '        </block>\n' +
-      '      </category>\n' +
-      '      <category name="Boolean">\n' +
-      '        <block type="logic_compare"></block>\n' +
-      '        <block type="logic_operation"></block>\n' +
-      '        <block type="logic_negate"></block>\n' +
-      '        <block type="logic_boolean"></block>\n' +
-      '        <block type="logic_null"></block>\n' +
-      '        <block type="logic_ternary"></block>\n' +
-      '      </category>\n' +
-      '    </category>\n' +
-      '    <category name="Loops">\n' +
-      '      <block type="controls_repeat_ext">\n' +
-      '        <value name="TIMES">\n' +
-      '          <block type="math_number">\n' +
-      '            <field name="NUM">10</field>\n' +
-      '          </block>\n' +
-      '        </value>\n' +
-      '      </block>\n' +
-      '      <block type="controls_whileUntil"></block>\n' +
-      '      <block type="controls_for">\n' +
-      '        <field name="VAR">i</field>\n' +
-      '        <value name="FROM">\n' +
-      '          <block type="math_number">\n' +
-      '            <field name="NUM">1</field>\n' +
-      '          </block>\n' +
-      '        </value>\n' +
-      '        <value name="TO">\n' +
-      '          <block type="math_number">\n' +
-      '            <field name="NUM">10</field>\n' +
-      '          </block>\n' +
-      '        </value>\n' +
-      '        <value name="BY">\n' +
-      '          <block type="math_number">\n' +
-      '            <field name="NUM">1</field>\n' +
-      '          </block>\n' +
-      '        </value>\n' +
-      '      </block>\n' +
-      '      <block type="controls_forEach"></block>\n' +
-      '      <block type="controls_flow_statements"></block>\n' +
-      '    </category>\n' +
-      '    <category name="Math">\n' +
-      '      <block type="math_number"></block>\n' +
-      '      <block type="math_arithmetic"></block>\n' +
-      '      <block type="math_single"></block>\n' +
-      '      <block type="math_trig"></block>\n' +
-      '      <block type="math_constant"></block>\n' +
-      '      <block type="math_number_property"></block>\n' +
-      '      <block type="math_round"></block>\n' +
-      '      <block type="math_on_list"></block>\n' +
-      '      <block type="math_modulo"></block>\n' +
-      '      <block type="math_constrain">\n' +
-      '        <value name="LOW">\n' +
-      '          <block type="math_number">\n' +
-      '            <field name="NUM">1</field>\n' +
-      '          </block>\n' +
-      '        </value>\n' +
-      '        <value name="HIGH">\n' +
-      '          <block type="math_number">\n' +
-      '            <field name="NUM">100</field>\n' +
-      '          </block>\n' +
-      '        </value>\n' +
-      '      </block>\n' +
-      '      <block type="math_random_int">\n' +
-      '        <value name="FROM">\n' +
-      '          <block type="math_number">\n' +
-      '            <field name="NUM">1</field>\n' +
-      '          </block>\n' +
-      '        </value>\n' +
-      '        <value name="TO">\n' +
-      '          <block type="math_number">\n' +
-      '            <field name="NUM">100</field>\n' +
-      '          </block>\n' +
-      '        </value>\n' +
-      '      </block>\n' +
-      '      <block type="math_random_float"></block>\n' +
-      '    </category>\n' +
-      '    <category name="Lists">\n' +
-      '      <block type="lists_create_empty"></block>\n' +
-      '      <block type="lists_create_with"></block>\n' +
-      '      <block type="lists_repeat">\n' +
-      '        <value name="NUM">\n' +
-      '          <block type="math_number">\n' +
-      '            <field name="NUM">5</field>\n' +
-      '          </block>\n' +
-      '        </value>\n' +
-      '      </block>\n' +
-      '      <block type="lists_length"></block>\n' +
-      '      <block type="lists_isEmpty"></block>\n' +
-      '      <block type="lists_indexOf"></block>\n' +
-      '      <block type="lists_getIndex"></block>\n' +
-      '      <block type="lists_setIndex"></block>\n' +
-      '    </category>\n' +
-      '    <category name="Variables" custom="VARIABLE"></category>\n' +
-      '    <category name="Functions" custom="PROCEDURE"></category>' +
-      '</xml>'
+    '<xml id="toolbox" style="display: none">' +
+    '<block type="Start"></block>' +
+    '<block type="Up"></block>' +
+    '<block type="Down"></block>' +
+    '<block type="Left"></block>' +
+    '<block type="Right"></block>' +
+    '<block type="Loop"></block>' +
+    '<block type="PenUp"></block>' +
+    '<block type="PenDown"></block>' +
+    '<block type="PenColor"></block>' +
+    '<block type="CircleRight"></block>' +
+    '<block type="CircleLeft"></block>' +
+    '<block type="Pause"></block>' +
+    '</xml>'
   };
 
   @ViewChild('blocklyDiv')
